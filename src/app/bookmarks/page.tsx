3ee0e5a08c073a46278d143c9045c8fbb6e6ffe2
kolby-k <kolby.k16@gmail.com>
2025-09-06 12:43:24 -0600

    Initalize Next app: - Add initial pages for bookmarks, sign-in, and summarize;

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+export default function Bookmarks() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-4xl font-bold text-red-500">Hello Jimbo</h1>
+      <h1 className="text-4xl font-bold text-red-500">
+        Review a list of bookmarks
+      </h1>
     </div>
   );
 }
